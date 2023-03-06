9a143b96ab51433879869fd9129839ab8663d629
Amber Oglesby <aoglesby5795@gmail.com>
2023-03-06 14:49:22 -0500

    add map to loadscript, add map origin and destination coordinates

--- frontend/static/src/APITestFile/APITest.js
+++ frontend/static/src/APITestFile/APITest.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { LoadScript } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  LoadScript,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+
+import { DirectionsService } from "@react-google-maps/api";
 
 const secret_key = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 //testing to see if API key returns
@@ -8,6 +14,17 @@ const secret_key = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 function APITest() {
   const [distance, setDistance] = useState(0);
   const [duration, setDuration] = useState(0);
+  const [directions, setDirections] = useState(0);
+
+  const containerStyle = {
+    width: "600px",
+    height: "600px",
+  };
+
+  const center = {
+    lat: 29.9511,
+    lng: -90.0715,
+  };
 
   useEffect(() => {
     if (distance && duration) {
@@ -20,17 +37,23 @@ function APITest() {
     const google = window.google;
     const directionsService = new google.maps.DirectionsService();
 
+    const origin = { lat: 29.959786, lng: -90.006175 };
+    const destination = { lat: 31.197533, lng: -89.205635 };
+
     directionsService.route(
       {
-        origin: "Jackson Barracks, LA",
-        destination: "Camp Shelby, MS",
+        origin: origin,
+        destination: destination,
         travelMode: google.maps.TravelMode.DRIVING,
       },
       (result, status) => {
         if (status === google.maps.DirectionsStatus.OK) {
           const directions = result.routes[0].legs[0].steps;
 
-          console.log(directions.map((x) => x.maneuver));
+          setDirections(directions.map((x) => x.maneuver));
+
+          console.log(directions);
+          //setDirections(result.routes[0].legs[0].steps);
           setDistance(result.routes[0].legs[0].distance.value);
           setDuration(result.routes[0].legs[0].duration.value);
         } else {
@@ -70,10 +93,22 @@ function APITest() {
         loadingElement={<div />}
         containerElement={<div />}
         mapElement={<div />}
-        onLoad={onLoad}
       >
         <div>Distance: {distance}</div>
         <div>Duration: {duration}</div>
+        <div>Directions: {directions}</div>
+
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          directions={directions}
+          distance={distance}
+          onLoad={onLoad}
+        >
+          {/* Child components, such as markers, info windows, etc.
+          <DirectionsRenderer directions={this.state.directions} /> */}
+        </GoogleMap>
       </LoadScript>
     </>
   );
