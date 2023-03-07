ab0458bbc40faf54a0bd836e60af9775e4a18539
Amber Oglesby <aoglesby5795@gmail.com>
2023-03-06 21:46:03 -0500

    add google maps marker, add form to input direction

--- frontend/static/src/APITestFile/APITest.js
+++ frontend/static/src/APITestFile/APITest.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
+import Autocomplete from "react-google-autocomplete";
+import { DirectionsService } from "@react-google-maps/api";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
+import { useMemo } from "react";
 import {
+  useJsApiLoader,
   GoogleMap,
   LoadScript,
+  MarkerF,
   DirectionsRenderer,
 } from "@react-google-maps/api";
 
-import { DirectionsService } from "@react-google-maps/api";
-
 const secret_key = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+
 //testing to see if API key returns
 //console.log(secret_key);
 
@@ -15,16 +23,24 @@ function APITest() {
   const [distance, setDistance] = useState(0);
   const [duration, setDuration] = useState(0);
   const [directions, setDirections] = useState(0);
+  const [map, setMap] = useState(/** @type google.maps.Map */ (""));
+  const [direction, setDirection] = useState(null);
+
+  const libraries = ["places"];
 
   const containerStyle = {
     width: "600px",
     height: "600px",
+    margin: "100px",
   };
 
-  const center = {
-    lat: 29.9511,
-    lng: -90.0715,
-  };
+  const center = useMemo(
+    () => ({
+      lat: 29.9511,
+      lng: -90.0715,
+    }),
+    []
+  );
 
   useEffect(() => {
     if (distance && duration) {
@@ -34,6 +50,7 @@ function APITest() {
 
   const onLoad = React.useCallback(function callback(map) {
     // Get directions
+
     const google = window.google;
     const directionsService = new google.maps.DirectionsService();
 
@@ -48,6 +65,7 @@ function APITest() {
       },
       (result, status) => {
         if (status === google.maps.DirectionsStatus.OK) {
+          //setDirections(result.routes[0].legs[0].steps);
           const directions = result.routes[0].legs[0].steps;
 
           setDirections(directions.map((x) => x.maneuver));
@@ -94,10 +112,6 @@ function APITest() {
         containerElement={<div />}
         mapElement={<div />}
       >
-        <div>Distance: {distance}</div>
-        <div>Duration: {duration}</div>
-        <div>Directions: {directions}</div>
-
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
@@ -106,10 +120,31 @@ function APITest() {
           distance={distance}
           onLoad={onLoad}
         >
+          <MarkerF position={center} />
+
           {/* Child components, such as markers, info windows, etc.
-          <DirectionsRenderer directions={this.state.directions} /> */}
+          <DirectionsRenderer directions={this.state.directions} />
+          <Marker position={destination} /> */}
         </GoogleMap>
       </LoadScript>
+      <Form.Group className="mb-3">
+        <Form.Label>Origin</Form.Label>
+
+        <Form.Control type="text" placeholder="Origin" />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Destination</Form.Label>
+
+        <Form.Control type="text" placeholder="Destination" />
+      </Form.Group>
+      <Card style={{ width: "18rem" }}>
+        <ListGroup variant="flush">
+          <ListGroup.Item>Distance: {distance}</ListGroup.Item>
+          <ListGroup.Item>Duration:{duration} </ListGroup.Item>
+          <ListGroup.Item>Directions:{directions}</ListGroup.Item>
+        </ListGroup>
+      </Card>
+      <Button variant="secondary">Start Navigation</Button>{" "}
     </>
   );
 }
--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -5,15 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div>
           <APITest />
-          Learn React
-        </a>
+        </div>
       </header>
     </div>
   );
